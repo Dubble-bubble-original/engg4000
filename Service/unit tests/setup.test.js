abee15a0f67f9c240767ebbf9f9710c491c75de0
Sahil Saini <32021832+sahilsaini432@users.noreply.github.com>
2021-09-28 10:50:39 -0300

    eslint and jest setup for backend (#10)
    * eslint and jest setup
    
    * Updated jest test script names
    
    Co-authored-by: Sahil <sahil@modelar.ai>
    Co-authored-by: Frederic Verret <43451024+fredy20501@users.noreply.github.com>

---
+++ Service/unit tests/setup.test.js
@@ -0,0 +1,8 @@
+const value = 5 * 5;
+
+// Inital test to check jest functionality
+describe('Initial Test', () => {
+    test('5 * 5 should be 25', () => {
+        expect(5*5).toBe(25);
+    });
+});
--- application/src/App.js
+++ application/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
@@ -16,8 +15,6 @@ function App() {
         >
           Learn React
         </a>
-        <br/>
-        <Button>Bootsrap button</Button>
       </header>
     </div>
   );
