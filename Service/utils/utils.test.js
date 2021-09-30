4577d99910e6a2efaa01080a684b63e1e9284418
NathanielCaron <nathaniel.caron22@gmail.com>
2021-09-30 13:37:47 -0300

    Added test to cover 50,000 case for removeStaleTokens function, moved initialization into before each statement

--- Service/utils/utils.test.js
+++ Service/utils/utils.test.js
@@ -3,10 +3,12 @@ const UTILS = require('../utils/utils');
 
 // Tests for removeStaleTokens function
 describe('removeStaleTokens', () => {
-    global.auth_tokens = new Map();
-    global.auth_tokens.set('a', 1);
-    global.auth_tokens.set('b', 2);
-    global.auth_tokens.set('c', 3);
+    beforeEach(() => {
+        global.auth_tokens = new Map();
+        global.auth_tokens.set('a', 1);
+        global.auth_tokens.set('b', 2);
+        global.auth_tokens.set('c', 3);
+    });
 
     test('token is removed when it is in the map', () => {
         UTILS.removeStaleTokens('b');
@@ -18,7 +20,17 @@ describe('removeStaleTokens', () => {
     test('map is unchanged when token not in the map', () => {
         UTILS.removeStaleTokens('d');
 
-        expect(global.auth_tokens.size).toBe(2);
+        expect(global.auth_tokens.size).toBe(3);
+    });
+
+    test('map is cleared when size is larger than 50,000', () => {
+        for (let i = 0; i < 50000; i++) {
+            global.auth_tokens.set(i, 1);
+        }
+
+        UTILS.removeStaleTokens('a');
+
+        expect(global.auth_tokens.size).toBe(0);
     });
 });
 
