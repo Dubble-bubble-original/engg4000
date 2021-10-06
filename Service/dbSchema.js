966d1de160150498484f37add2cd8079816410cb
JoaoPontes98 <joao.vrpontes@gmail.com>
2021-10-06 11:39:34 -0300

    Fixed linting issues from prev commit

--- Service/dbSchema.js
+++ Service/dbSchema.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const postSchema = new Schema({
-    date_created: {type: Date, default: Date.now},
-    body: {type: String},
-    tags: [{type: String}],
-    title: {type: String},
-    imgURL: {type: String}
+  date_created: { type: Date, default: Date.now },
+  body: { type: String },
+  tags: [{ type: String }],
+  title: { type: String },
+  imgURL: { type: String }
 });
-const Post = mongoose.model('Post', postSchema, "Post");
+const Post = mongoose.model('Post', postSchema, 'Post');
 
-module.exports = {Post};
+module.exports = { Post };
 
-//Check the Documentation folder for best practices on developing with schemas
+//  Check the Documentation folder for best practices on developing with schemas
