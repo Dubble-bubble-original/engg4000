707c5fa16779262ebfbee0b5445e1b70ca16c377
Frederic Verret <43451024+fredy20501@users.noreply.github.com>
2021-11-27 18:37:20 -0400

    Create NavBar component boilerplate

---
+++ application/src/components/NavBar.js
@@ -0,0 +1,21 @@
+// React
+import PropTypes from 'prop-types'
+
+// Stylesheet
+import './components.css';
+
+function NavBar() {
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+NavBar.propTypes = {
+    placeholder: PropTypes.string
+}
+
+export default NavBar;
