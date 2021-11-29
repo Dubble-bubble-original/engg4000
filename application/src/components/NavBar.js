c602def477a4472a2d3f6086281c99ad5380262c
Frederic Verret <43451024+fredy20501@users.noreply.github.com>
2021-11-29 10:19:03 -0400

    Add basic navbar

--- application/src/components/NavBar.js
+++ application/src/components/NavBar.js
@@ -1,5 +1,6 @@
 // React
 import PropTypes from 'prop-types'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 
 // Stylesheet
 import './components.css';
@@ -8,9 +9,25 @@ function NavBar() {
 
 
   return (
-    <div>
-
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
