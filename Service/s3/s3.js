3adb7733c6d8e0f7cc80db818ef3bb605f8640c0
nathanielcaron <nathaniel.caron22@gmail.com>
2021-11-30 11:26:50 -0400

    Removed unwanted import statement in S3.js

--- Service/s3/s3.js
+++ Service/s3/s3.js
@@ -1,6 +1,5 @@
 const S3 = require('aws-sdk/clients/s3');
 const fs = require('fs');
-const logger = require('../../application/src/logger/logger');
 require('dotenv').config();
 const ENV = process.env;
 
