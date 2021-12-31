9ca5e018efb661443f775d83877183d37d849577
Frederic Verret <43451024+fredy20501@users.noreply.github.com>
2021-12-31 15:41:38 -0400

    Changed hamburger menu to a popup

--- application/src/components/navbar/NavBar.js
+++ application/src/components/navbar/NavBar.js
@@ -1,14 +1,43 @@
 // React
 import PropTypes from 'prop-types'
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown, Offcanvas } from 'react-bootstrap';
 
 // Resources
 import './navBar.css';
 import {IconContext} from 'react-icons';
 import {MdHome, MdSearch, MdAddLocation, MdMoreHoriz } from 'react-icons/md';
 import Logo from '../../resources/images/nota-logo-no-text.png';
+import {FRow} from '../FlexContainers'
+
+function NavLinks(props) {
+  // Handler to call closePopup() if it was given when clicking a link
+  function clickHandler() {
+    if (props.closePopup) props.closePopup();
+  }
+
+  return (
+    <IconContext.Provider value={{color:'#EC4038'}}>
+      <Nav className="me-auto">
+          <Nav.Link onClick={() => clickHandler(props.setContent('home'))} className={props.content === 'home' ? 'active' : ''}>Home <MdHome/></Nav.Link>
+          <Nav.Link onClick={() => clickHandler(props.setContent('search'))} className={props.content === 'search' ? 'active' : ''}>Search <MdSearch/></Nav.Link>
+          <Nav.Link onClick={() => clickHandler(props.setContent('create'))} className={props.content === 'create' ? 'active' : ''}>Create <MdAddLocation/></Nav.Link>
+      </Nav>
+      <Nav className="justify-content-end">
+        <NavDropdown align="end" title={<MdMoreHoriz/>} id="basic-nav-dropdown">
+          <NavDropdown.Item onClick={() => clickHandler(props.setShowTerms(true))}>Terms and Conditions</NavDropdown.Item>
+          <NavDropdown.Item onClick={() => clickHandler(props.setContent('delete'))}>Delete a post</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </IconContext.Provider>
+  );
+}
 
 function NavBar(props) {
+
+  // Function to close the offcanvas popup
+  function closePopup() {
+    document.querySelector('#offcanvasNavbar button.btn-close').click();
+  }
 
   return (
     <Navbar bg="light" expand="lg" id="navbar" className="h5 navbar-expand-sm">
@@ -19,25 +48,35 @@ function NavBar(props) {
             alt="Nota logo"
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <IconContext.Provider value={{color:'#EC4038'}}>
-            <Nav className="me-auto">
-                <Nav.Link onClick={() => props.setContent('home')} className={props.content === 'home' ? 'active' : ''}>Home <MdHome/></Nav.Link>
-                <Nav.Link onClick={() => props.setContent('search')} className={props.content === 'search' ? 'active' : ''}>Search <MdSearch/></Nav.Link>
-                <Nav.Link onClick={() => props.setContent('create')} className={props.content === 'create' ? 'active' : ''}>Create <MdAddLocation/></Nav.Link>
-            </Nav>
-            <Nav className="justify-content-end">
-              <NavDropdown align="end" title={<MdMoreHoriz/>} id="basic-nav-dropdown">
-                <NavDropdown.Item onClick={() => props.setShowTerms(true)}>Terms and Conditions</NavDropdown.Item>
-                <NavDropdown.Item onClick={() => props.setContent('delete')}>Delete a post</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </IconContext.Provider>
-        </Navbar.Collapse>
+
+        <FRow id="nav-link-container" className="basic-navbar-nav">
+          <NavLinks {...props}/>
+        </FRow>
+
+        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Offcanvas
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="end"
+          className="navbar-light"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="offcanvasNavbarLabel">Menu</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body id="nav-link-container" className="h5">
+            <NavLinks {...props} closePopup={closePopup}/>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Container>
     </Navbar>
   )
+}
+
+NavLinks.propTypes = {
+  content: PropTypes.string,
+  setContent: PropTypes.func,
+  setShowTerms: PropTypes.func,
+  closePopup: PropTypes.func
 }
 
 NavBar.propTypes = {
