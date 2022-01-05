8984ec0213872480d2973861b92a7d6a45113259
Frederic Verret <43451024+fredy20501@users.noreply.github.com>
2022-01-05 17:35:41 -0400

    Add basic fields to delete page

--- application/src/components/Delete.js
+++ application/src/components/Delete.js
@@ -1,15 +1,28 @@
 // React
-import { Container } from 'react-bootstrap';
+import { Container, Form, Row, Col, Button } from 'react-bootstrap';
 
 function Delete() {
 
   return (
     <div>
       {/* Temporary content (change me) */}
-      <Container style={{width: 1234}}>
-        <br/>
-        <b>Delete component goes here.</b><br/>
-        <br/>
+      <Container className="outer-container">
+        <Form>
+          <div className="h4">Delete a post</div>
+          <div>To delete a post you must use the access code that was given to you when you created the post.</div>
+          <br/>
+          <Row className="align-items-end">
+            <Col>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Access code</Form.Label>
+                <Form.Control type="email" placeholder="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Button>Search</Button>
+            </Col>
+          </Row>
+        </Form>
       </Container>
     </div>
   )
