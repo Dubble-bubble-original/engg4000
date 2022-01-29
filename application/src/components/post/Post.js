46d49c09a2b356546cde4f93ae1198f601e53c05
Frederic Verret <43451024+fredy20501@users.noreply.github.com>
2022-01-29 13:31:32 -0400

    Add error checking for post images

--- application/src/components/post/Post.js
+++ application/src/components/post/Post.js
@@ -1,4 +1,5 @@
 // React
+import { useState } from 'react';
 import { Container, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types'
 import { format } from 'date-fns'
@@ -11,7 +12,18 @@ import StaticMap from '../maps/StaticMap';
 import { FRow, FCol } from '../FlexContainers';
 import PlaceholderAvatar from '../../resources/images/placeholder-avatar.png';
 
+// Show placeholder image if avatar image fails
+function handleAvatarImgError(e) {
+  if (e.target.src != PlaceholderAvatar) e.target.src = PlaceholderAvatar;
+}
+
 function Post({postData}) {
+  const [imgURL, setimgURL] = useState(postData.img_url);
+
+  // Show no image if post picture fails
+  const handlePictureImgError = () => {
+    setimgURL(null);
+  }
 
   // Create Tags for rendering
   const tags = postData.tags.sort().map(tag => {
@@ -26,7 +38,7 @@ function Post({postData}) {
     <Container className="outer-container">
       <FRow>
         <FCol>
-          <img className="avatar" src={postData.author.avatar_url ?? PlaceholderAvatar} />
+          <img className="avatar" src={postData.author.avatar_url ?? PlaceholderAvatar} onError={handleAvatarImgError} />
           <div className="user-name text-center">{postData.author.name}</div>
         </FCol>
 
@@ -48,8 +60,8 @@ function Post({postData}) {
               <div className="text-muted text-center">{postData.location_string}</div>
             </FCol>
           </FRow>
-          <div hidden={!postData.img_url}>
-            <img src={postData.img_url} />
+          <div hidden={!imgURL}>
+            <img src={imgURL} onError={handlePictureImgError} />
           </div>
         </FCol>
       </FRow>
