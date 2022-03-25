e120761cfe3ad9347e46aa42e02a508c5d4d5821
Frederic Verret <43451024+fredy20501@users.noreply.github.com>
2022-03-25 13:35:00 -0300

    Fix tests for navbar

--- application/src/components/navbar/NavBar.test.js
+++ application/src/components/navbar/NavBar.test.js
@@ -1,6 +1,8 @@
 import { render, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import NavBar from './NavBar';
+import { HashRouter as Router } from 'react-router-dom'
 
 // Clear everything after each clean up
 afterEach(cleanup);
@@ -13,28 +15,31 @@ const props = {
 
 describe('Nav Bar Tests', () => {
   test('render NavBar', () => {
-    render(< NavBar content={ 'home' } setContent={ props.setContent } setShowTerms={ props.setShowTerms }/>);
+    render(<Router>< NavBar setShowTerms={ props.setShowTerms }/></Router>);
     expect(screen.getByTestId('nav-home-btn')).toBeVisible();
     expect(screen.getByTestId('nav-search-btn')).toBeVisible();
     expect(screen.getByTestId('nav-create-btn')).toBeVisible();
   });
 
   test('check home page is active', () => {
-    render(< NavBar content={ 'home' } setContent={ props.setContent } setShowTerms={ props.setShowTerms }/>);
+    render(<Router>< NavBar setShowTerms={ props.setShowTerms }/></Router>);
+    userEvent.click(screen.getByTestId('nav-home-btn'));
     expect(screen.getByTestId('nav-home-btn')).toHaveClass('active');
     expect(screen.getByTestId('nav-search-btn')).not.toHaveClass('active');
     expect(screen.getByTestId('nav-create-btn')).not.toHaveClass('active');
   });
 
   test('check search page is active', () => {
-    render(< NavBar content={ 'search' } setContent={ props.setContent } setShowTerms={ props.setShowTerms }/>);
+    render(<Router>< NavBar setShowTerms={ props.setShowTerms }/></Router>);
+    userEvent.click(screen.getByTestId('nav-search-btn'));
     expect(screen.getByTestId('nav-home-btn')).not.toHaveClass('active');
     expect(screen.getByTestId('nav-search-btn')).toHaveClass('active');
     expect(screen.getByTestId('nav-create-btn')).not.toHaveClass('active');
   });
 
   test('check create page is active', () => {
-    render(< NavBar content={ 'create' } setContent={ props.setContent } setShowTerms={ props.setShowTerms }/>);
+    render(<Router>< NavBar setShowTerms={ props.setShowTerms }/></Router>);
+    userEvent.click(screen.getByTestId('nav-create-btn'));
     expect(screen.getByTestId('nav-home-btn')).not.toHaveClass('active');
     expect(screen.getByTestId('nav-search-btn')).not.toHaveClass('active');
     expect(screen.getByTestId('nav-create-btn')).toHaveClass('active');
