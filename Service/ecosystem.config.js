bdc81dd8fc768aa5d4f04ec588c672645b8697f9
Sahil Saini <sainisahil432@gmail.com>
2022-03-29 10:27:28 -0300

    Test 62

--- Service/ecosystem.config.js
+++ Service/ecosystem.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  apps : [{
-    name   : "Service",
-    script : "./app.js",
-    exec_mode: 'cluster',
-    instances: '2',
-    node_args: ["--max_old_space_size=1024"],
-    max_memory_restart: "1G",
+  apps: [{
+    name: 'Service',
+    script: './app.js',
+    node_args: ['--max_old_space_size=1024'],
+    max_memory_restart: '1G',
     listen_timeout: 10000,
     kill_timeout: 15000
   }]
-}
+};
