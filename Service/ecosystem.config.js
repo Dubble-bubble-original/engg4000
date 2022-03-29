a867141e2261b7b700ea149b08933c1383b3fc29
nathanielcaron <42100702+nathanielcaron@users.noreply.github.com>
2022-03-29 18:28:30 -0300

    Updated service settings to use more memory

--- Service/ecosystem.config.js
+++ Service/ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [{
     name: 'Service',
     script: './app.js',
-    node_args: ['--max_old_space_size=1024'],
-    max_memory_restart: '1G',
+    node_args: ['--max_old_space_size=1536'],
+    max_memory_restart: '1536M',
     listen_timeout: 10000,
     kill_timeout: 15000
   }]
