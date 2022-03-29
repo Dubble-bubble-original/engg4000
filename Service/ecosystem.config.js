ac6671b62943ad538f99e710a6539e55ace20f9e
nathanielcaron <42100702+nathanielcaron@users.noreply.github.com>
2022-03-29 18:35:35 -0300

    Increased memory again

--- Service/ecosystem.config.js
+++ Service/ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [{
     name: 'Service',
     script: './app.js',
-    node_args: ['--max_old_space_size=1536'],
-    max_memory_restart: '1536M',
+    node_args: ['--max_old_space_size=2560'],
+    max_memory_restart: '2560M',
     listen_timeout: 10000,
     kill_timeout: 15000
   }]
